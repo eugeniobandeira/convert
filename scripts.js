f8610fce18862a3fdcd465cc2258ed5ebdd07957
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-01-02 19:48:07 -0300

    conclusão

--- scripts.js
+++ scripts.js
@@ -1,1 +1,56 @@
-console.log("inicado");
+const exchangeRates = {
+    USD: 5.5,
+    EUR: 6.5,
+    GBP: 8.2
+};
+
+const form = document.querySelector('form');
+const footer = document.querySelector('main footer');
+const amount = document.getElementById('amount');
+const currency = document.getElementById('currency');
+const description = document.getElementById('description');
+const result = document.getElementById('result');
+
+amount.addEventListener('input', () => {
+    const hasCharacteresRegex = /\D+/g;
+    amount.value = amount.value.replace(hasCharacteresRegex, '');
+});
+
+form.onsubmit = (event) => {
+    event.preventDefault();
+
+    switch (currency.value) {
+        case 'USD':
+            convertCurrency(amount.value, exchangeRates.USD, '$');
+            break;
+        case 'EUR':
+            convertCurrency(amount.value, exchangeRates.EUR, '€');
+            break;
+        case 'GBP':
+            convertCurrency(amount.value, exchangeRates.GBP, '£');
+            break;
+        default:
+            alert('Moeda não suportada');
+    }
+}
+
+function convertCurrency(amount, price, symbol) {
+    try {
+        description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`;
+
+        let total = formatCurrencyBRL(amount * price);
+        result.textContent = `${total}`;
+
+        footer.classList.add('show-result');
+    } catch (error) {
+        footer.classList.remove('show-result');
+        console.error(error);
+    }
+}
+
+function formatCurrencyBRL(value) {
+    return Number(value).toLocaleString("pr-BR", {
+        style: "currency",
+        currency: "BRL",
+    });
+}
